adc78c4eaf8500dbdc9df2d83e1cc914f21d1879
Kristina Tikvicki <kikitiki1989@gmail.com>
2021-04-09 21:39:56 +0200

    proba

---
+++ taxi-sluzba/src/korisnici/Korisnik.java
@@ -0,0 +1,105 @@
+package korisnike;
+
+public class Korisnik {
+    private String jmbg;
+    private String korisnickoIme;
+    private String lozinka;
+    private String ime;
+    private String prezime;
+    private String adresa;
+    private String pol;
+    private String brojTelefona;
+
+    public Korisnik(){
+
+    }
+
+    public Korisnik(String jmbg, String korisnickoIme, String lozinka, String ime, String prezime, String adresa, String pol, String brojTelefona) {
+        this.jmbg = jmbg;
+        this.korisnickoIme = korisnickoIme;
+        this.lozinka = lozinka;
+        this.ime = ime;
+        this.prezime = prezime;
+        this.adresa = adresa;
+        this.pol = pol;
+        this.brojTelefona = brojTelefona;
+    }
+
+    public String getJmbg() {
+        return jmbg;
+    }
+
+    public void setJmbg(String jmbg) {
+        this.jmbg = jmbg;
+    }
+
+    public String getKorisnickoIme() {
+        return korisnickoIme;
+    }
+
+    public void setKorisnickoIme(String korisnickoIme) {
+        this.korisnickoIme = korisnickoIme;
+    }
+
+    public String getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getPol() {
+        return pol;
+    }
+
+    public void setPol(String pol) {
+        this.pol = pol;
+    }
+
+    public String getBrojTelefona() {
+        return brojTelefona;
+    }
+
+    public void setBrojTelefona(String brojTelefona) {
+        this.brojTelefona = brojTelefona;
+    }
+
+    @Override
+    public String toString() {
+        return "Korisnik{" +
+                "jmbg='" + jmbg + '\'' +
+                ", korisnickoIme='" + korisnickoIme + '\'' +
+                ", lozinka='" + lozinka + '\'' +
+                ", ime='" + ime + '\'' +
+                ", prezime='" + prezime + '\'' +
+                ", adresa='" + adresa + '\'' +
+                ", pol='" + pol + '\'' +
+                ", brojTelefona='" + brojTelefona + '\'' +
+                '}';
+    }
+}
--- test/src/korisnike/Korisnik.java
+++
@@ -1,2 +0,0 @@
-package korisnike;public class Korisnik {
-}
